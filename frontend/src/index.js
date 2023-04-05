9b69079ac1069cd2ec16b7463de283ee2adb8bc6
Trung Nguyen <rezolden@gmail.com>
2023-04-05 00:38:38 -0400

    change font, favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "@fontsource/roboto-slab";
+// import "@fontsource/roboto-slab";
 import "@fontsource/jetbrains-mono"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
